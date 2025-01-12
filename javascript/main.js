eeecc5a66b555fe4f3500cdc6def0c6c5caec86e
Gideon Simon <simonsamuel195@gmail.com>
2025-01-12 02:00:10 +0100

    working on responsiveness

--- javascript/main.js
+++ javascript/main.js
@@ -90,3 +90,18 @@ document.addEventListener("DOMContentLoaded", function() {
         animate();
     });
 });
+
+const hamburger = document.querySelector('.menu-hamburger')
+const menu = document.querySelector('.menu')
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active')
+  menu.classList.toggle('active')
+})
+
+document.querySelectorAll('.menu-link').forEach(link => 
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active')
+    menu.classList.remove('active')
+  })
+)
